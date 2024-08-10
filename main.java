c59266d16839c6823a0f5c300b09647db248666f
Joshua Grant Kana <61407084+Grant6551@users.noreply.github.com>
2024-08-10 20:28:27 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+class Base {
+	int a = 10, b = 20;
+
+	void displayAB() {
+		System.out.println("In Base Class: " + a + " | " + b);
+	}
+}
+
+class Derived extends Base {
+	int c = 30;
+
+	void displayABC() {
+		System.out.println("In Derived Class: " + a + " | " + b + " | " + c);
+	}
+}
+
+public class main {
+	public static void main(String[] args) {
+		// create a child class instance
+		Derived obj = new Derived();
+		System.out.println(obj.a + " | " + obj.b + " | " + obj.c);
+		obj.displayAB();
+		obj.displayABC();
+	}
+}
